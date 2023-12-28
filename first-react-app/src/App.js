036a2d6b19bfe037d336bb13d8d6d4bee2a805ed
b2aprogramming <96809207+b2aprogramming@users.noreply.github.com>
2023-12-28 11:14:16 +0530

    react start up

---
+++ first-react-app/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+// NVM DOWNLOAD LINK: https://github.com/coreybutler/nvm-windows/releases
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello React App how are you</h1>
+    </div>
+  );
+}
+
+export default App;
