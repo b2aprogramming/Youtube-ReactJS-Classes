5fb82f8820b0b68080397306e1ba9b1bc716be3b
b2aprogramming <96809207+b2aprogramming@users.noreply.github.com>
2024-05-18 17:00:43 +0530

    ts models added

--- react-app-typescript/src/TestTs.ts
+++ react-app-typescript/src/TestTs.ts
@@ -1,3 +1,5 @@
+import internal from "stream";
+
 interface AddressDetails {
     address1: string;
     address2: string;
@@ -99,3 +101,213 @@ function forEach2(arr: any[], forEachFun: (ele: any, index: number, a: any[]) =>
     }
     
 }
+
+// ======= enums ==========
+
+enum Satus {
+    Active = 'active',
+    InActive = 'inactive',
+    Pending = 'pending',
+    Completed = 'completed'
+};
+
+enum Satus2 {
+    Active,
+    InActive,
+    Pending,
+    Completed
+};
+
+
+const status = Satus.Active;
+
+//console.log('@@@ status',status)
+
+// var qb = 10;
+// var er = 20;
+
+// (function(x: number, y: number){ // 
+//     console.log('@@@ FUNCTION', (x + y))
+// })(qb,er);
+
+var Sta;
+(function(stat){ // 
+    console.log('@@@ FUNCTION', (stat))
+})(Sta || (Sta = {}));
+
+var aa = Satus2.InActive;
+console.log('@@@ AAA',aa)
+
+var res = 0;
+
+if(res === Satus2.Active) {
+
+}
+
+// ======== Keyvalue pair =======
+
+interface KeyValuePair {
+    [key: string]: string | number | boolean | object;
+    // name: string;
+}
+
+const obj2: KeyValuePair = {
+    name: '',
+    id: '',
+    number: 23,
+    details: {
+        name: '',
+        address: ''
+    }
+};
+
+const arr2: number[] = [];
+const arr3: Array<number> = [3,5,6,7];
+
+
+interface HttpReponse<T>{
+    status: number;
+    data: T; 
+    // data: {
+    //     name: string;
+    //     age: number;
+    
+    // };
+    message: string;
+}
+
+interface UserData {
+    name: string;
+    age: number;
+}
+
+function getResponse(resp: HttpReponse<UserData>){
+
+}
+
+function getResponse2(resp: HttpReponse<{name: string, id: number}>){
+   // resp.data.
+}
+
+function getResponse3(resp: HttpReponse<number[]>){
+   // resp.data.
+  // resp.data
+}
+// getResponse();
+
+function getResponse4<T>(resp: HttpReponse<T>){
+    // resp.data.
+   // resp.data
+   
+ }
+
+ getResponse4<number[]>({
+    status: 200,
+    data: [1,2,4,5,6,7,8,9],
+    message: 'success'
+ });
+
+ interface UserDetails {
+    name: string;
+    age: number;
+    address: string;
+ }
+
+ interface UserDetails2 {
+    role?: string
+ }
+
+ interface EmployeDetails extends UserDetails, UserDetails2{
+    id: number;
+    email: string;
+    phone: string;
+    // name: string;
+    // age: number;
+    // address: string;
+ }
+
+ const tt: EmployeDetails = {
+    address: 'Hyd',
+    name: 'Hyd',
+    id: 2,
+    phone: 'Hyd',
+    age: 6,
+    email: 'Hyd',
+    role: 'Hyd'
+ };
+
+
+ interface MyDataModel {
+    name: string;
+    age: number;
+    address: string;
+    phone: string;
+    email: string;
+ }
+
+ const myData1: Partial<MyDataModel> = {
+   name: 'str'
+ }
+
+ interface Props {
+  a?: number;
+  b?: string;
+}
+
+const myData2: Props = {
+    a: 10,
+   // b: 'sag'
+}
+
+const myData3: Required<Props> = {
+    a: 10,
+    b: 'sag'
+}
+
+const myData4: Readonly<Props> = {
+    a: 10,
+    b: 'sag'
+}
+
+interface Props2 {
+    a: Readonly<number>;
+    b?: string;
+  }
+const myData5: Props2 = {
+    a: 10,
+    b: 'sag'
+}
+myData5.b = 'sagsag';
+myData5.a = 40;
+
+
+interface StauValueModel {
+    name: string;
+    value: number;
+}
+   
+type StatusName = "active" | "inactive" | "completed" | "pending";
+
+const obj5: Record<StatusName, StauValueModel> = {
+    active: {
+        name: 'active', value: 1
+    },
+    inactive: {
+        name: 'inactive', value: 2
+    },
+    completed: {
+        name: 'completed', value: 3
+    },
+    pending: {
+        name: 'pending', value: 4
+    }
+}
+
+// obj5.active
+ 
+ 
+
+
+
+
+
