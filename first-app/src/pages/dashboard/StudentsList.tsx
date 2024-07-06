f0943e4c2623cba176e5f4ec382d50c61bc2b307
b2aprogramming <96809207+b2aprogramming@users.noreply.github.com>
2024-07-06 10:15:37 +0530

    create students code added

--- first-app/src/pages/dashboard/StudentsList.tsx
+++ first-app/src/pages/dashboard/StudentsList.tsx
@@ -6,6 +6,13 @@ import DialogContent from '@mui/material/DialogContent';
 import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
 import Slide from '@mui/material/Slide';
+import dayjs from 'dayjs';
+
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+
+
 import { TransitionProps } from '@mui/material/transitions';
 
 import Icon from "../../shared/components/icon/Icon";
@@ -23,10 +30,22 @@ const Transition = React.forwardRef((
   return <Slide direction="down" ref={ref} {...props} />;
 });
 
+const formFields = {
+    name: '',
+    className: '',
+    avatar: '',
+    email: '',
+    classSection: '',
+    joiningDate: dayjs(new Date()),
+    parentName: '',
+    phone: '',
+    city: '',
+};
 
 function Studentslist() {
     const [openModal, setOpenModal] = React.useState(false);
-    const [students, setStudetns] = useState([]);
+    const [students, setStudetns] = useState<any>([]);
+    const [stuendtForm, setStudentForm] = useState(formFields);
 
     const getStudetns = () => {
         fetch('http://localhost:3000/students').then((res) => {
@@ -47,6 +66,41 @@ function Studentslist() {
 
     const handleClose = () => {
         setOpenModal(false);
+    };
+
+    const submitForm = () => {
+       // setOpenModal(false);
+       console.log('@@@ FORM', stuendtForm)
+       let id = (+students[students.length-1].id) + 1;
+       const payload = {
+        ...stuendtForm,
+        joiningDate: stuendtForm.joiningDate.format('DD/MM/YYYY'),
+        id: id
+       };
+
+       fetch('http://localhost:3000/students', {
+        method: 'POST',
+        body: JSON.stringify(payload)
+       }).then((res) => {
+        res.json().then((data) => {
+            console.log('@@@ save students', data);
+            setOpenModal(false);
+            setStudentForm(formFields);
+        })
+        
+    }).catch((err) => {
+        console.log('@@@ ERR', err);
+    })
+    };
+
+    const changeInput = (evt: React.ChangeEvent<HTMLInputElement>, formField: string) => {
+        setStudentForm((prev: any) => {
+            const fields = {
+                ...prev
+            };
+            fields[formField] = evt.target.value;
+            return fields;
+        })
     };
 
     useEffect(() => {
@@ -108,7 +162,9 @@ function Studentslist() {
                                     id="outlined-required"
                                     label="Name"
                                     defaultValue=""
+                                    value={stuendtForm.name}
                                     sx={{width: '100%'}}
+                                    onChange={(evt: any) => changeInput(evt, 'name')}
                                 />
                             </div>
                             <div className="form-group">
@@ -116,8 +172,35 @@ function Studentslist() {
                                     required
                                     id="outlined-required"
                                     label="Email"
+                                    value={stuendtForm.email}
                                     defaultValue=""
                                     sx={{width: '100%'}}
+                                    onChange={(evt: any) => changeInput(evt, 'email')}
+                                />
+                            </div>
+                        </div>
+
+                        <div className="form-field-row">
+                            <div className="form-group">
+                                <TextField
+                                    required
+                                    id="outlined-required"
+                                    label="Phone"
+                                    value={stuendtForm.phone}
+                                    defaultValue=""
+                                    sx={{width: '100%'}}
+                                    onChange={(evt: any) => changeInput(evt, 'phone')}
+                                />
+                            </div>
+                            <div className="form-group">
+                                <TextField
+                                    required
+                                    id="outlined-required"
+                                    label="Parent Name"
+                                    value={stuendtForm.parentName}
+                                    defaultValue=""
+                                    sx={{width: '100%'}}
+                                    onChange={(evt: any) => changeInput(evt, 'parentName')}
                                 />
                             </div>
                         </div>
@@ -129,7 +212,9 @@ function Studentslist() {
                                     id="outlined-required"
                                     label="Class Name"
                                     defaultValue=""
+                                    value={stuendtForm.className}
                                     sx={{width: '100%'}}
+                                    onChange={(evt: any) => changeInput(evt, 'className')}
                                 />
                             </div>
                             <div className="form-group">
@@ -137,8 +222,34 @@ function Studentslist() {
                                     required
                                     id="outlined-required"
                                     label="Classs Section"
+                                    value={stuendtForm.classSection}
                                     defaultValue=""
                                     sx={{width: '100%'}}
+                                    onChange={(evt: any) => changeInput(evt, 'classSection')}
+                                />
+                            </div>
+                        </div>
+                        <div className="form-field-row">
+                            <div className="form-group">
+
+                            <LocalizationProvider dateAdapter={AdapterDayjs}>
+                            <DatePicker 
+                                label="Joining Date"
+                                sx={{width: '100%'}}
+                                value={stuendtForm.joiningDate}
+                                onChange={(evt: any) => changeInput(evt, 'joiningDate')}
+                            />
+                            </LocalizationProvider>
+                            </div>
+                            <div className="form-group">
+                                <TextField
+                                    required
+                                    id="outlined-required"
+                                    label="City"
+                                    value={stuendtForm.city}
+                                    defaultValue=""
+                                    sx={{width: '100%'}}
+                                    onChange={(evt: any) => changeInput(evt, 'city')}
                                 />
                             </div>
                         </div>
@@ -149,7 +260,7 @@ function Studentslist() {
                 </DialogContent>
                 <DialogActions>
                 <Button variant="outlined" onClick={handleClose}>Cancel</Button>
-                <Button variant="contained" onClick={handleClose}>Submit</Button>
+                <Button variant="contained" onClick={submitForm}>Submit</Button>
                 </DialogActions>
             </Dialog>
         </section>
